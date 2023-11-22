9caefb4d056ebcf1209ea2cd6c059c811d45d873
SaraRosena <141441965+SaraRosena@users.noreply.github.com>
2023-11-22 13:21:55 -0300

    ProjetoWebsite

--- ProjetoWebsite/index.js
+++ ProjetoWebsite/index.js
@@ -1,24 +1,7 @@
-const express = require('express')
-const app = express()
-const exphbs = require('express-handlebars')
+const express = require('express');
+const app = express();
 const port = 3000
 
-app.engine('handlebars',exphbs.engine())
-app.set('view engine','handlebars')
-
-app.use(
-  express.urlencoded({
-    extended: true
-  })
-  )
-
-app.use(express.json())
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-res.render('home')
-})
-
 app.listen(port, () => {
-console.log('Server Started')
+    console.log(`Meu servidor está rodando na porta ${port}`)
 })
